66ccf6326efa36e94f134421d1b00d14c7deaa49
Pavel Lavrinovich <lavrinovicpavel417@gmail.com>
2026-05-20 13:18:08 +0300

    Added anecdotes exercise

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [maxVotes, setMaxVotes] = useState(0);
+  const [mostVotedAnecdoteIndex, setMostVotedAnecdoteIndex] = useState(0);
+
+  // getting random index
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+
+  // vote functionality and saving largest vote number
+  function handleVote() {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
+
+    if (newVotes[selected] > maxVotes){
+      setMaxVotes(newVotes[selected]);
+      setMostVotedAnecdoteIndex(selected)
+    }
+  }
+  console.log(votes);
+
+
+  
+  
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={() => setSelected(getRandomNumber(0, anecdotes.length - 1))}>Next anecdote</button>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[mostVotedAnecdoteIndex]}</p>
+    </div>
+  );
+};
+
+export default App;
