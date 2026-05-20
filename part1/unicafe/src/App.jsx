b3a15bbae076ca96e2ba0c6b842c2ad8c01056f4
Pavel Lavrinovich <lavrinovicpavel417@gmail.com>
2026-05-20 03:56:28 +0300

    Add unicafe starred exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 const Header = ({ header }) => {
-  return <h2>{header}</h2>
-}
+  return <h2>{header}</h2>;
+};
 
 const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>
-}
+  return <button onClick={onClick}>{text}</button>;
+};
 
 const StatisticLine = ({ text, value }) => {
-  return <p>{text} {value}</p>
-}
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
 
 const Statistics = (props) => {
-
-  const { good, neutral, bad } = props
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = good / all * 100
+  const { good, neutral, bad } = props;
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
 
   if (all === 0) {
     return (
@@ -26,56 +29,93 @@ const Statistics = (props) => {
         <Header header={"statistics"} />
         <p>No feedbacks given</p>
       </>
-    )
+    );
   }
 
   return (
-    <>
-      <Header header={"statistics"} />
-      <StatisticLine text={"good"} value={good} />
-      <StatisticLine text={"neutral"} value={neutral} />
-      <StatisticLine text={"bad"} value={bad} />
-      <StatisticLine text={"all"} value={all} />
-      <StatisticLine text={"average"} value={Number.isFinite(average) ? average : '0'} />
-      <StatisticLine text={"positive"} value={Number.isFinite(positive) ? positive + ' %' : '0 %'} />
-    </>
-  )
-}
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <Header header={"statistics"} />
+          </th>
+        </tr>
+      </thead>
+      {/* <Header header={"statistics"} /> */}
+      <tbody colSpan={3}>
+        <StatisticLine text={"good"} value={good} />
+        <StatisticLine text={"neutral"} value={neutral} />
+        <StatisticLine text={"bad"} value={bad} />
+        <StatisticLine text={"all"} value={all} />
+        <StatisticLine
+          text={"average"}
+          value={Number.isFinite(average) ? average : "0"}
+        />
+        <StatisticLine
+          text={"positive"}
+          value={Number.isFinite(positive) ? positive + " %" : "0 %"}
+        />
+      </tbody>
+    </table>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const incrementGood = () => {
-    const newValue = good + 1
+    const newValue = good + 1;
     console.log("incremented Good, now it's: " + newValue);
-    setGood(newValue)
-  }
+    setGood(newValue);
+  };
 
   const incrementNeutral = () => {
-    const newValue = neutral + 1
+    const newValue = neutral + 1;
     console.log("incremented Neutral, now it's: " + newValue);
-    setNeutral(newValue)
-  }
+    setNeutral(newValue);
+  };
 
   const incrementBad = () => {
-    const newValue = bad + 1
+    const newValue = bad + 1;
     console.log("incremented Bad, now it's: " + newValue);
-    setBad(newValue)
-  }
+    setBad(newValue);
+  };
 
   return (
     <div>
-      <Header header={'give feedback'} />
-      <Button onClick={incrementGood} text={'good'} />
-      <Button onClick={incrementNeutral} text={'neutral'} />
-      <Button onClick={incrementBad} text={'bad'} />
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={3}>
+              <Header header={"give feedback"} />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Button onClick={incrementGood} text={"good"} />
+            </td>
+            <td>
+              <Button onClick={incrementNeutral} text={"neutral"} />
+            </td>
+            <td>
+              <Button onClick={incrementBad} text={"bad"} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <Statistics good={good} neutral={neutral} bad={bad} />
+      {/* <Header header={'give feedback'} /> */}
 
+      {/* <Button onClick={incrementGood} text={'good'} /> */}
+      {/* <Button onClick={incrementNeutral} text={'neutral'} /> */}
+      {/* <Button onClick={incrementBad} text={'bad'} /> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
